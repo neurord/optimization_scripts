324df5eb38180ae487ea32f4ed93d3f98bb7c5bc
Avrama Blackwell <avrama@gmu.edu>
2019-12-19 15:40:07 -0500

    plot_opt_results: generalize for fitting different neuron types

--- plot_opt_results.py
+++ plot_opt_results.py
@@ -3,7 +3,7 @@ from ajustador.helpers.copy_param import create_model_from_param as cmfp
 import numpy as np
 import os
 import glob
-import gpedata_experimental as gpe
+import importlib
 from matplotlib import pyplot as plt
 plt.ion()
 
@@ -20,9 +20,8 @@ def plot_history(fitvals,dataname):
     plt.xlabel('evaluation')
     plt.ylabel('fitness')
     return
-def plot_traces(bestdir,dataname):
-    exp_to_fit = gpe.alldata[dataname+'-2s'][[0,2,4]]
-    traces=glob.glob(bestdir+'/*.npy')
+
+def plot_traces(bestdir,dataname,traces,exp_to_fit):
     plt.figure()
     for wave in exp_to_fit.waves:
         plt.plot(wave.wave.x,wave.wave.y)
@@ -35,12 +34,33 @@ def plot_traces(bestdir,dataname):
     plt.xlabel('Time')
     plt.ylabel('Vm')
     return
+
 #################### main ############
 ###### Specify directory that has output directory with cmaes/fit*.npz files in it.
+'''
 target_dir='gp_opt/output/'
-## Must specify morphfile if opt script specified morphfile different than what is in param_cond.py
-morph= 'GP_soma.p'
+wave_module='gpedata_experimental'
+wave_set=[0,2,4]
 
+target_dir='Str_opt/output'
+wave_module='A2Acre'
+wave_set={'FSI':[1, 5, 14, 17, 20], 'D1': [1, 5, 14, 17, 22], 'D2': [1, 5, 12, 15, 19]}
+'''
+target_dir='epopt-all/output'
+wave_module='EPdata'
+wave_set=[0,2,5,7]
+
+#different data naming conventions for different wave modules
+if wave_module.startswith('A2A'):
+    suffix='_'+npz_file.split('cmaes_')[1].split('_')[1]
+elif wave_module.startswith('gp'):
+    suffix='-2s'
+else:
+    suffix=''
+## Must specify morphfile if opt script specified morphfile different than what is in param_cond.py
+morph= None
+
+wavedir=importlib.import_module(wave_module)
 os.chdir(target_dir)
 pattern='cmaes*/fit*.npz'
 fnames=glob.glob(pattern)
@@ -49,15 +69,25 @@ for npz_file in fnames:
     fitvals,tmpdirs=load_npz(npz_file)
     bestfit=np.argmin(fitvals[:,-1])
     bestdir=tmpdirs[bestfit]
-    dataname=npz_file.split('cmaes_')[1].split('_')[0]
-    neuron_type=npz_file.split('/')[1].split('-')[1]
     model=npz_file.split('/fit')[1].split('-')[0]
-
-    plot_history(fitvals,dataname)
-    plot_traces(bestdir,dataname)
-    text=input('generate moose-nerp package (y/n)')
-    if text=='n':
-        print ("Next one")
+    neuron_type=npz_file.split('/fit')[1].split('-')[1]
+    dataname=npz_file.split('cmaes_')[1].split('_')[0]
+    if isinstance(wave_set,list):
+        exp_to_fit = wavedir.alldata[dataname+suffix][wave_set]
+    elif isinstance(wave_set,dict):
+        exp_to_fit = wavedir.alldata[dataname+suffix][wave_set[neuron_type]]
     else:
-        cmfp.createNewModelFolder(model, neuron_type, npz_file, dataname+'_1comp',morphfile=morph)
+        print('create new condition for specifying list of waves')
+    plot_history(fitvals,dataname)
+    traces=glob.glob(bestdir+'/*.npy')
+    print('num traces',len(traces), 'num waves', len(exp_to_fit))
+    if len(traces):
+        plot_traces(bestdir,dataname,traces,exp_to_fit)
+        text=input('generate moose-nerp package (y/n)')
+        if text=='n':
+            print ("Next one")
+        else:
+            cmfp.createNewModelFolder(model, neuron_type, npz_file, dataname+'_1comp',morphfile=morph,copy_traces=True)
+    else:
+        print('>>>> fit directorywith traces {} does not exist'.format(dataname))
    
